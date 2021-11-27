5b187661942f27874da55deedc787cc7f23e7d4e
Francisco-Fuenzalida <francisco.fuenzalida.parra@gmail.com>
2021-11-27 11:45:50 -0300

    ComboBox medico en listar_horas

--- AppGalenos/views.py
+++ AppGalenos/views.py
@@ -4,22 +4,30 @@ from .forms import HorasForm
 
 # Create your views here.
 
+
 def return_home(request):
-    return render(request,"home.html")
+    return render(request, "home.html")
+
 
 def listar_hora(request):
     horas = Hora.objects.all()
-    return render(request, "listar_horas.html", {'horas': horas})
-
-def add_hora(request):
-    
     medicos = Medico.objects.all()
-
-    data={
-        'form' : HorasForm(),
+    data = {
+        'horas': horas,
         'medicos': medicos
     }
-    
+    return render(request, "listar_horas.html", data)
+
+
+def add_hora(request):
+
+    medicos = Medico.objects.all()
+
+    data = {
+        'form': HorasForm(),
+        'medicos': medicos
+    }
+
     if request.method == 'POST':
         formulario = HorasForm(data=request.POST, files=request.FILES)
         if formulario.is_valid():
@@ -27,24 +35,27 @@ def add_hora(request):
             data["mensaje"] = "guardado correctamente"
         else:
             data["form"] = formulario
-    return render(request, "add_hora.html", data)    
+    return render(request, "add_hora.html", data)
+
 
 def borrar_hora(request, id_hora):
     instancia = Hora.objects.get(id=id_hora)
     instancia.delete()
     return redirect('listar_horas')
 
+
 def editar_hora(request, id_hora):
     hora = get_object_or_404(Hora, id=id_hora)
-    
+
     data = {
-        'form' : HorasForm(instance=hora)
+        'form': HorasForm(instance=hora)
     }
-    
+
     if request.method == 'POST':
-        formulario = HorasForm(data=request.POST, instance=hora, files=request.FILES)
+        formulario = HorasForm(
+            data=request.POST, instance=hora, files=request.FILES)
         if formulario.is_valid():
             formulario.save()
-            return redirect (to="listar_horas")
+            return redirect(to="listar_horas")
         data["form"] = formulario
     return render(request, "horas/editar_hora.html", data)
